2828a208b04555a3a6a905b0165f32f2fda61fb5
James Holland <jamesfholland@gmail.com>
2015-09-09 23:13:03 -0600

    Added some more settings.

--- src/Settings.java
+++ src/Settings.java
@@ -4,6 +4,23 @@
 public interface Settings
 {
 
-  int MINIMUM_NUMBER_ROOMS = 6;
+  /**
+   * Number of rooms to generate.
+   */
+  int DEFAULT_NUMBER_ROOMS = 6;
 
+  /**
+   * Number of Hallways to generate.
+   */
+  int DEFAULT_NUMBER_HALLS = 4;
+
+  /**
+   * Number of random obstacles.
+   */
+  int DEFAULT_NUMBER_OBSTACLES = 5;
+
+  /**
+   * Chance of firetrap in a given floor tile.
+   */
+  double DEFAULT_FIRETRAP_CHANCE = 0.10; //10%
 }
