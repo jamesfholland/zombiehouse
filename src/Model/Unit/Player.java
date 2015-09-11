0a7ae9c97ee5bdc31413d8b5e5b67ae101d95b6f
James Holland <jamesfholland@gmail.com>
2015-09-10 19:42:44 -0600

    Added image to player

--- src/Model/Unit/Player.java
+++ src/Model/Unit/Player.java
@@ -2,17 +2,32 @@ package Model.Unit;
 
 import Model.GameObject;
 
+import javax.imageio.ImageIO;
 import javax.sound.sampled.Clip;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Player extends Unit
 {
+  private static BufferedImage playerFront;
 
   public Player(Point location)
   {
     this.location = location;
     this.size = new Dimension(60, 70);
+
+    if(playerFront == null)
+    {
+      try
+      {
+        playerFront = ImageIO.read(this.getClass().getResourceAsStream("playerFront.png"));
+      }
+      catch (IOException e)
+      {
+        e.printStackTrace();
+      }
+    }
 
   }
 
