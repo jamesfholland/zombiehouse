6ed1e56e9dac0b18daedffa1653d20a8385bb336
Julian Weisburd <jmweisburd@unm.edu>
2015-09-19 14:22:12 -0600

    changed variable name so that there's no conflicts

--- src/Model/Unit/Zombie/Zombie.java
+++ src/Model/Unit/Zombie/Zombie.java
@@ -13,7 +13,7 @@ public abstract class Zombie extends Unit
   protected static double smell = 7.0;
 
   protected double heading;
-  protected Point direction;
+  protected Point vector;
   protected Point testPoint;
 
   protected boolean collided = false;
@@ -24,7 +24,7 @@ public abstract class Zombie extends Unit
   public Zombie(int x, int y, double heading)
   {
     this.location = new Point(x,y);
-    this.direction = new Point(0,0);
+    this.vector = new Point(0,0);
     this.testPoint = new Point(0,0);
     this.heading = heading;
     this.size = new Dimension(50,70);
@@ -48,50 +48,50 @@ public abstract class Zombie extends Unit
   {
     if (heading == 0.0)
     {
-      direction.x = 1;
-      direction.y = 0;
+      vector.x = 1;
+      vector.y = 0;
     }
 
     else if (heading > 0.0 && heading < 90.0)
     {
-      direction.x = 1;
-      direction.y = -1;
+      vector.x = 1;
+      vector.y = -1;
     }
 
     else if (heading == 90.0)
     {
-      direction.x = 0;
-      direction.y =-1;
+      vector.x = 0;
+      vector.y =-1;
     }
 
     else if (heading > 90.0 && heading < 180.0)
     {
-      direction.x = -1;
-      direction.y = -1;
+      vector.x = -1;
+      vector.y = -1;
     }
 
     else if (heading == 180.0)
     {
-      direction.x = -1;
-      direction.y = 0;
+      vector.x = -1;
+      vector.y = 0;
     }
 
     else if (heading >180.0 && heading < 270.0)
     {
-      direction.x = -1;
-      direction.y = 1;
+      vector.x = -1;
+      vector.y = 1;
     }
 
     else if (heading == 270.0)
     {
-      direction.y = 1;
-      direction.x = 0;
+      vector.y = 1;
+      vector.x = 0;
     }
 
     else if (heading >270.0 && heading < 360)
     {
-      direction.y = 1;
-      direction.x = 1;
+      vector.y = 1;
+      vector.x = 1;
     }
   }
 
