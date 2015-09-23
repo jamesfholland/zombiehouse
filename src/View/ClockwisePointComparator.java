fa89ed538c9b506c9958c70bbee63a07ab45b497
James Holland <jamesfholland@gmail.com>
2015-09-23 17:22:58 -0600

    Fix for the point comparator. Now uses arctan2 to compare angles.

--- src/View/ClockwisePointComparator.java
+++ src/View/ClockwisePointComparator.java
@@ -1,21 +1,26 @@
 package View;
 
+import java.awt.*;
 import java.awt.geom.Point2D;
 import java.util.Comparator;
 
 /**
  * This class compares Point2D objects in order to sort them in a clockwise fashion around a center point.
  */
-class ClockwisePointComparator implements Comparator<Point2D>
+class ClockwisePointComparator implements Comparator<Point>
 {
-  private Point2D center;
-  ClockwisePointComparator(Point2D center)
+  private Point center;
+
+  ClockwisePointComparator(Point center)
   {
     this.center = center;
   }
+
   @Override
-  public int compare(Point2D pointA, Point2D pointB)
+  public int compare(Point pointA, Point pointB)
   {
-    return (int)((pointA.getX() - center.getX())*(pointB.getY() - center.getY()) - (pointB.getX() - center.getX()) * (pointA.getY() - center.getY()));
+    return Double.compare(
+        Math.atan2(pointA.y - center.y, pointA.x - center.x),
+        Math.atan2(pointB.y - center.y, pointB.x - center.x));
   }
 }
