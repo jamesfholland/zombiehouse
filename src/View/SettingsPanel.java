9f3d754ba9d5ee3fa5db80cd6efd4b145e94f7f5
James Holland <jamesfholland@gmail.com>
2015-09-27 04:13:23 -0600

    Added blank Settings Panel. Will add to Jdialog eventually

---
+++ src/View/SettingsPanel.java
@@ -0,0 +1,14 @@
+package View;
+
+import javax.swing.*;
+
+/**
+ * SettingsPanel allows the user to change default settings in the application.
+ */
+public class SettingsPanel extends JPanel
+{
+  SettingsPanel()
+  {
+
+  }
+}
