a0e388323b683f1c8049db987d3e614e592af3c8
James Holland <jamesfholland@gmail.com>
2015-09-27 15:43:22 -0600

    Added burn to wall images

--- src/Model/Tile/Wall.java
+++ src/Model/Tile/Wall.java
@@ -1,5 +1,7 @@
 package Model.Tile;
 
+import Model.Settings;
+
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -8,20 +10,32 @@ import java.io.IOException;
 public class Wall extends Tile
 {
   private final static BufferedImage WALL_IMAGE;
+  private final static BufferedImage WALL_BURNED_IMAGE;
 
   static
   {
-    BufferedImage imageTemp = null;
+    BufferedImage imageWall = null;
+    BufferedImage imageBurn = null;
 
     try
     {
-      imageTemp = ImageIO.read(Wall.class.getResourceAsStream("wood.png"));
+      imageWall = ImageIO.read(Pillar.class.getResourceAsStream("wood.png"));
+      imageBurn = ImageIO.read(Pillar.class.getResourceAsStream("burn.png"));
     }
     catch (IOException e)
     {
       e.printStackTrace();
     }
-    WALL_IMAGE = imageTemp;
+    Graphics graphics;
+    //Basic Floor
+    WALL_IMAGE = new BufferedImage(Settings.TILE_SIZE, Settings.TILE_SIZE, BufferedImage.TYPE_INT_ARGB);
+    graphics = WALL_IMAGE.getGraphics();
+    graphics.drawImage(imageWall, 0, 0, Settings.TILE_SIZE, Settings.TILE_SIZE, null);
+    //Burned Floor
+    WALL_BURNED_IMAGE = new BufferedImage(Settings.TILE_SIZE, Settings.TILE_SIZE, BufferedImage.TYPE_INT_ARGB);
+    graphics = WALL_BURNED_IMAGE.getGraphics();
+    graphics.drawImage(imageWall, 0, 0, Settings.TILE_SIZE, Settings.TILE_SIZE, null);
+    graphics.drawImage(imageBurn, 0, 0, Settings.TILE_SIZE, Settings.TILE_SIZE, null);
   }
 
   private boolean markedForDeletion;
@@ -37,9 +51,13 @@ public class Wall extends Tile
   @Override
   public BufferedImage getImage()
   {
+    if (burned)
+    {
+      return WALL_BURNED_IMAGE;
+    }
+
     return WALL_IMAGE;
   }
-
   @Override
   public boolean isWall()
   {
