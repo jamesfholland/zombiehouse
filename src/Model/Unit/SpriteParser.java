3334d717a23c23738f382d0a71f1b48b598882d5
James Holland <jamesfholland@gmail.com>
2015-09-27 00:49:03 -0600

    Added multiline sprite parser. This is needed by fire.

--- src/Model/Unit/SpriteParser.java
+++ src/Model/Unit/SpriteParser.java
@@ -23,4 +23,23 @@ public class SpriteParser
 
     return returnArray;
   }
+
+  public static BufferedImage[] parseMultiRowSprites(BufferedImage sheet, int numberRows, int spriteHeight, int spriteWidth, int xOffset, int yOffset, int spriteCount)
+  {
+
+    BufferedImage[] returnArray = new BufferedImage[spriteCount*numberRows];
+    BufferedImage tempImage;
+    for(int spriteRow = 0; spriteRow < numberRows; spriteRow++)
+    {
+      for (int i = 0; i < spriteCount; i++)
+      {
+        tempImage = sheet.getSubimage(i * spriteWidth, spriteRow * spriteHeight, spriteWidth, spriteHeight);
+
+        //-2*xOffset to get both sides trimmed
+        returnArray[spriteRow*spriteCount + i] = tempImage.getSubimage(xOffset, yOffset, spriteWidth - 2 * xOffset, spriteHeight - yOffset);
+      }
+    }
+
+    return returnArray;
+  }
 }
