67956ab9b2be51d524813f69ed2b2d38c6a39d5e
James Holland <jamesfholland@gmail.com>
2015-09-29 00:18:44 -0600

    Javadocced the PointComparator class.

--- src/View/ClockwisePointComparator.java
+++ src/View/ClockwisePointComparator.java
@@ -4,27 +4,38 @@ import java.awt.*;
 import java.util.Comparator;
 
 /**
- * This class compares Point2D objects in order to sort them in a clockwise fashion around a center point.
+ * This class compares Point objects in order to sort them in a clockwise fashion around a CENTER point.
+ * This is a utility class used when ordering points in shadow detection.
  */
 class ClockwisePointComparator implements Comparator<Point>
 {
-  private Point center;
+  private final Point CENTER;
 
+  /**
+   * Sets up a new comparator with CENTER to sort around.
+   * @param center
+   */
   ClockwisePointComparator(Point center)
   {
-    this.center = center;
+    this.CENTER = center;
   }
 
+  /**
+   * Compare two points angles from the predetermined CENTER
+   * @param pointA
+   * @param pointB
+   * @return -1 to 1 if pointA is less than to greater than pointB on a clockwise angle.  If both angles are the same, it returns a Double.compare of the distanceSq(A,B)
+   */
   @Override
   public int compare(Point pointA, Point pointB)
   {
     int angleCompare = Double.compare(
-        Math.atan2(pointA.y - center.y, pointA.x - center.x),
-        Math.atan2(pointB.y - center.y, pointB.x - center.x));
+        Math.atan2(pointA.y - CENTER.y, pointA.x - CENTER.x),
+        Math.atan2(pointB.y - CENTER.y, pointB.x - CENTER.x));
 
     if (angleCompare == 0)
     {
-      return Double.compare(center.distanceSq(pointA), center.distanceSq(pointB));
+      return Double.compare(CENTER.distanceSq(pointA), CENTER.distanceSq(pointB));
     }
     return angleCompare;
   }
